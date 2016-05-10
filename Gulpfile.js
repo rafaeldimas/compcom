ef13bd3f29a2ad0ebf92c806b978bca09be2df94
citotame <rafael_citotame@hotmail.com>
2016-05-10 04:03:38 -0300

    Finalição de implementação do task runner Gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,66 +1,66 @@
+var filesJS = [
+"./src/js/*.js",
+"./src/imports/owl-carousel/*.js"
+];
+
+var filesCSS = [
+"./src/css/*.css",
+"./src/imports/owl-carousel/*.css"
+];
+
+var filesHTML = "./src/*.html";
+
+// Núcleo do Gulp
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var cleanCSS = require('gulp-clean-css');
+
+// Transforma o javascript em formato ilegível para humanos
+var uglify = require("gulp-uglify");
+
+// Agrupa todos os arquivos em um
+var concat = require("gulp-concat");
+
+// Verifica alterações em tempo real, caso haja, compacta novamente todo o projeto 
+var watch = require('gulp-watch');
+
+// Minifica o CSS
+var cssmin = require("gulp-cssmin");
+
+// Remove comentários CSS
+var stripCssComments = require('gulp-strip-css-comments');
+
+//Minifica Html
 var htmlmin = require('gulp-htmlmin');
 
-
-var filesJS = "./src/js/*.js";
-var filesCSS = "./src/css/*.css";
-var filesCarrocel = "./src/imports/owl-carousel/*.css";
-
-gulp.task('lint', function() {
-  gulp.src(filesJS)
-  .pipe(jshint())
-  .pipe(jshint.reporter('default'));
-});
-
-gulp.task('dist-js', function() {
-  gulp.src(filesJS)
-  .pipe(concat('./dist/js'))
-  .pipe(rename('all.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('./dist/js'));
-});
-
-gulp.task('dist-carrocel', function() {
-  gulp.src('./src/imports/owl-carousel/*.js')
-  .pipe(concat('./dist/js'))
-  .pipe(rename('carrocel.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('./dist/js'));
-});
-
-gulp.task('dist-css', function() {
-  gulp.src(filesCarrocel)
-  .pipe(concat('./dist/css'))
-  .pipe(rename('carrocel.min.css'))
-  .pipe(cleanCSS({debug: true}, function(details) {
-    console.log(details.name + ': ' + details.stats.originalSize);
-    console.log(details.name + ': ' + details.stats.minifiedSize);
-  }))
-  .pipe(gulp.dest('./dist/css'));
-})
-gulp.task('minify-css', function() {
+// Processo que agrupará todos os arquivos CSS, removerá comentários CSS e minificará.
+gulp.task('minify-css', function(){
   gulp.src(filesCSS)
-  .pipe(cleanCSS({debug: true}, function(details) {
-    console.log(details.name + ': ' + details.stats.originalSize);
-    console.log(details.name + ': ' + details.stats.minifiedSize);
-  }))
-  .pipe(gulp.dest('./dist/css'));
+  .pipe(concat('style.min.css'))
+  .pipe(stripCssComments({all: true}))
+  .pipe(cssmin())
+  .pipe(gulp.dest('./dist/css/'));
+});
+
+// Tarefa de minificação do Javascript
+gulp.task('minify-js', function () {
+  gulp.src(filesJS)                        // Arquivos que serão carregados, veja variável 'js' no início
+  .pipe(concat('script.min.js'))      // Arquivo único de saída
+  .pipe(uglify())                     // Transforma para formato ilegível
+  .pipe(gulp.dest('./dist/js/'));          // pasta de destino do arquivo(s)
 });
 
 gulp.task('minify-html', function() {
-  gulp.src('./src/index.html')
+  gulp.src(filesHTML)
   .pipe(htmlmin({collapseWhitespace: true}))
   .pipe(gulp.dest('./dist'))
 });
 
-gulp.task('default', function() {
-  gulp.run('lint', 'dist-js','dist-carrocel', 'dist-css', 'minify-css', 'minify-html');
-  gulp.watch('./src/*', function(evt) {
-    gulp.run('lint', 'dist-js','dist-carrocel', 'dist-css', 'minify-css', 'minify-html');
-  });
+// Tarefa padrão quando executado o comando GULP
+gulp.task('default',['minify-js','minify-css','minify-html']);
+
+// Tarefa de monitoração caso algum arquivo seja modificado, deve ser executado e deixado aberto, comando "gulp watch".
+gulp.task('watch', function() {
+  gulp.watch(filesJS, ['minify-js']);
+  gulp.watch(filesCSS, ['minify-css']);
+  gulp.watch(filesHTML, ['minify-html']);
 });
+
